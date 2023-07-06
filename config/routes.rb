e20d198feeb77aa923dee9fba4db0f580a8a094f
ねず <reborn.dance1985@gmail.com>
2023-07-07 05:29:59 +0900

    タイムラインのテンプレートを用意

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
+  resource :timeline, only: [:show]
 
   resources :articles do
     resources :comments, only: [:new, :create]
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
   end
 
   resource :profile, only: [:show, :edit, :update]
